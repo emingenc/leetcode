64d7ae153e4d3ba0f82a61ba603394f76d882aeb
ck-unifr <chenk.ck03@gmail.com>
2018-05-17 09:22:19 +0200

    update solutions

---
+++ algorithms/16-3sum-closest.py
@@ -0,0 +1,40 @@
+# https://leetcode.com/problems/3sum-closest/description/#
+
+# Given an array nums of n integers and an integer target,
+# find three integers in nums such that the sum is closest to target.
+# Return the sum of the three integers. You may assume that each input would have exactly one solution.
+
+# Example:
+# Given array nums = [-1, 2, 1, -4], and target = 1.
+#
+# The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+
+
+# Fork from https://github.com/kamyu104/LeetCode/blob/master/Python/3sum-closest.py
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums, result, min_diff, i = sorted(nums), float("inf"), float("inf"), 0
+        while i < len(nums) - 2:
+            if i == 0 or nums[i] != nums[i - 1]:
+                j, k = i + 1, len(nums) - 1
+                while j < k:
+                    diff = nums[i] + nums[j] + nums[k] - target
+                    if abs(diff) < min_diff:
+                        min_diff = abs(diff)
+                        result = nums[i] + nums[j] + nums[k]
+                    if diff < 0:
+                        j += 1
+                    elif diff > 0:
+                        k -= 1
+                    else:
+                        return target
+            i += 1
+        return result
+
+if __name__ == "__main__":
+    assert Solution().threeSumClosest([1, 1, 1, 1], -100) == 3
