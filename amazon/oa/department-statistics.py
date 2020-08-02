f596887af9ca30e30c3d56be7c591be75efb5928
Kai <chenk.ck03@gmail.com>
2020-08-02 23:05:38 +0200

    update questions

---
+++ amazon/oa/department-statistics.py
@@ -0,0 +1,70 @@
+"""
+https://www.lintcode.com/problem/department-statistics/description?_from=ladder&&fromId=156
+
+Description
+中文
+English
+You are given infomations of employees in a company, 
+with their IDs, names and the department they belong to.
+And their friendships, every friendship contains two IDs, 
+"1, 2" means employees ID 1 and ID 2 are friends.
+The friendships are not transitive, for example, 
+AandBare bothC's friend, howeverBandCmay not be friend.
+Please count the number of employees that have a friend in another department for every department.
+
+All input is followed by a space after the comma, 
+and the output of your program must be in the same format as the sample.
+The returned list has no order requirements.
+The size of employee infomation is no more than 50.
+The size of friendships is no more than 1000.
+The IDs are numbers no more than 100.
+The number of different department is no more than 20.
+
+Have you met this question in a real interview?  
+Clarification
+In the example，employee 1 in Engineer and employee 2 in HR are friends，
+employee 3 in Engineer and employee 4 in Businessare friends，
+so Engineer has 2 employees with other department，return "Engineer: 2 of 3“。
+Besides HR has 1， Business has 1.
+
+Example
+Sample Input:
+employees = [
+  "1, Bill, Engineer",
+  "2, Joe, HR",
+  "3, Sally, Engineer",
+  "4, Richard, Business",
+  "6, Tom, Engineer"
+]
+
+friendships = [
+  "1, 2",
+  "1, 3",
+  "3, 4"
+]
+Sample Output:
+"Engineer: 2 of 3"
+"HR: 1 of 1"
+"Business: 1 of 1"
+
+"""
+
+from collections import defaultdict
+
+
+class Solution:
+    def departmentStatistics(self, employees, friendships):
+        id2grp = {}
+        grp2tot = defaultdict(int)
+        grp2cnt = defaultdict(set)
+        for e in employees:
+            id, _, grp = e.split(', ')
+            id2grp[id] = grp
+            grp2tot[grp] += 1
+        for f in friendships:
+            id1, id2 = f.split(', ')
+            g1, g2 = id2grp[id1], id2grp[id2]
+            if g1 != g2:
+                grp2cnt[g1].add(id1)
+                grp2cnt[g2].add(id2)
+        return [g + ': ' + str(len(grp2cnt[g])) + ' of ' + str(grp2tot[g]) for g in grp2tot]
---
+++ amazon/oa/minimum-cost-to-connect-sticks.py
@@ -0,0 +1,61 @@
+"""
+https://www.lintcode.com/problem/minimum-cost-to-connect-sticks/description?_from=ladder&&fromId=156
+
+https://www.jiuzhang.com/problem/minimum-cost-to-connect-sticks/
+
+1872. Minimum Cost to Connect Sticks
+Description
+中文
+English
+You have some sticks with positive integer lengths.
+You can connect any two sticks of lengths X and Y into one stick by paying a cost of X + Y. 
+You perform this action until there is one stick remaining.
+Return the minimum cost of connecting all the given sticks into one stick in this way.
+
+1 \leq sticks.length \leq 10^41≤sticks.length≤10
+​4
+​​ 
+1 \leq sticks[i] \leq 10^41≤sticks[i]≤10
+​4
+​​ 
+Have you met this question in a real interview?  
+Example
+Example 1:
+
+Input:
+ [2,4,3]
+Output: 14
+Explanation: First connect 2 and 3 to 5 and cost 5; then connect 5 and 4 to 9; total cost is 14
+Example 2:
+
+Input:
+ [1,8,3,5]
+Output: 30
+
+"""
+
+import heapq
+
+
+class Solution:
+    """
+    @param sticks: the length of sticks
+    @return: Minimum Cost to Connect Sticks
+    """
+
+    def MinimumCost(self, sticks):
+        # write your code here
+        if not sticks:
+            return 0
+
+        queue = []  # 用来保持木棒的队列
+        for n in sticks:
+            heapq.heappush(queue, n)
+        cost = 0
+        for i in range(len(sticks)-1):  # 总共合并n-1次
+            a = heapq.heappop(queue)
+            b = heapq.heappop(queue)
+            cost += (a+b)
+            heapq.heappush(queue, (a+b))
+
+        return cost
