3a839b9969ee889cf831d85d44fa21fa3d00f4f1
Kai <chenk.ck03@gmail.com>
2020-08-18 00:11:02 +0200

    update questions

--- amazon/oa/zombie-in-matrix.py
+++ amazon/oa/zombie-in-matrix.py
@@ -1,7 +1,9 @@
 """
 https://leetcode.com/discuss/interview-question/411357/
 
-Given a 2D grid, each cell is either a zombie 1 or a human 0. Zombies can turn adjacent (up/down/left/right) human beings into zombies every hour. Find out how many hours does it take to infect all humans?
+Given a 2D grid, each cell is either a zombie 1 or a human 0. 
+Zombies can turn adjacent (up/down/left/right) human beings into zombies every hour. 
+Find out how many hours does it take to infect all humans?
 
 Example:
 
