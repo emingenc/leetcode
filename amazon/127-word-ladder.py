3de1af76ee880e2918e02abb805f7487e8ad2454
Kai <chenk.ck03@gmail.com>
2020-09-07 19:13:57 +0200

    update questions

--- amazon/127-word-ladder.py
+++ amazon/127-word-ladder.py
@@ -38,6 +38,8 @@ Explanation: The endWord "cog" is not in wordList, therefore no possible transfo
 
 """
 
+from typing import List
+
 
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
